0129c41ef93af4474c09d369d69016d4cc8bae99
JulietK <julietgisemba000@>
2020-09-26 17:57:04 +0300

    modified the files

---
+++ src/main/java/dao/FoodtypeDao.java
@@ -0,0 +1,21 @@
+package dao;
+import models.Foodtype;
+import java.util.List;
+
+public interface FoodtypeDao {
+
+    //create
+    void add (FoodtypeDao foodtypeDao);
+    //void addFoodtypeTpRestaurant(FoodtypeDao foodtypeDaO, Restaurant restaurant);
+
+    //read
+    List<Foodtype> getAll();
+    // List<Restaurant> getAllRestaurantsForAFoodtype(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
---
+++ src/main/java/dao/Sql2oRestaurantDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import models.Restaurant;
+import org.sql2o.Sql2o;
+
+public class Sql2oRestaurantDao {
+    public Sql2oRestaurantDao(Sql2o sql2o) {
+    }
+
+    public void add(Restaurant restaurant) {
+    }
+}
